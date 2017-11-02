c0497a3d9bbd2abeaaeaec48f1fd244292028e44
Paulo Rodrigues <paulorodriguesxv@gmail.com>
2017-11-02 18:28:39 -0200

    React

--- todo-list-example/src/index.js
+++ todo-list-example/src/index.js
@@ -1,16 +1,15 @@
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-*/
-
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './reducers'
+import App from './App'
 
 let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
